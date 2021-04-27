1423fb24d3d54716495ed1fb4eac42151af92fc4
pc-work <adipol13@gmail.com>
2021-04-27 14:31:31 -0400

    creacion de modulo usuario del admin

---
+++ app/Http/Livewire/AdminUsers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Entity;
+use App\Models\User;
+use Livewire\Component;
+
+class AdminUsers extends Component
+{
+    public function render()
+    {
+        $users = User::paginate(8);
+
+        return view('livewire.admin-users', compact('users'));
+    }
+}
--- config/adminlte.php
+++ config/adminlte.php
@@ -243,6 +243,13 @@ return [
             'text'        => 'Lista de roles',
             'route'         => 'admin.roles.index',
             'icon'        => 'fas fa-fw fa-users-cog',
+            'active' => ['admin/roles*']
+        ],
+        [
+            'text'        => 'Usuarios',
+            'route'         => 'admin.users.index',
+            'icon'        => 'fas fa-fw fa-users',
+            'active' => ['admin/users*']
         ],
         ['header' => 'account_settings'],
         [
@@ -429,5 +436,5 @@ return [
     | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
     */
 
-    'livewire' => false,
+    'livewire' => true,
 ];
