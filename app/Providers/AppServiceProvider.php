aa84d9f019f67e8dce33ae6e747a6bf0c3c7feb4
pc-work <adipol13@gmail.com>
2021-05-26 11:30:11 -0400

    Valores para carbon ok m

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('routeIs', function ($expression) {
             return "<?php if(Request::url() == route($expression)): ?>";
         });
+
+        Carbon::setLocale('es');
+        //Carbon::setUTF8(true);
+        setlocale(LC_TIME, 'Spanish');
     }
 }
