2275bcfa7c7b3e9f633c8816c389d8bec5c12705
pc-work <adipol13@gmail.com>
2021-05-26 14:15:54 -0400

    Visualiza allegados ok

--- app/Http/Controllers/Client/AllController.php
+++ app/Http/Controllers/Client/AllController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AllController extends Controller
 {
     public function show($id)
     {
-        return view('client.all.show');
+        $pep = DB::table('controls')->where('id_pep', $id)->first();
+        $all = DB::table('controls')->where('id_pep', $id)
+            ->where('type_pep', 'ALL')->get();
+
+        return view('client.all.show', compact('pep', 'all'));
     }
 }
